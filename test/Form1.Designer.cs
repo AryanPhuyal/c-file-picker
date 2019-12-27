ca0d20116629fd135b7cff537ef69c8cdc769222
aryan <phuyalrn2@gmail.com>
2019-12-27 21:06:40 +0545

    Add project files.

---
+++ test/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.but = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // but
+            // 
+            this.but.Location = new System.Drawing.Point(663, 263);
+            this.but.Name = "but";
+            this.but.Size = new System.Drawing.Size(75, 23);
+            this.but.TabIndex = 2;
+            this.but.Text = "button1";
+            this.but.UseVisualStyleBackColor = true;
+            this.but.Click += new System.EventHandler(this.but_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.Location = new System.Drawing.Point(522, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(266, 210);
+            this.button3.TabIndex = 1;
+            this.button3.Text = "momo";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.but);
+            this.Controls.Add(this.button3);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button but;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ test/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media.Imaging;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        private OpenFileDialog openFileDialog1;
+        List<String> l = new List<string>();
+        List<Button> lButton = new List<Button>();
+        private int top1 = 0;
+        private int left1 = 100;
+        private int left2 = 200;
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1 = new OpenFileDialog();
+        }
+
+
+      
+
+ 
+            // 
+       
+        public void buttonAdd()
+        {
+            
+            Button button1 = new Button();
+            Button button2 = new Button();
+
+            button1.Left = left1;
+            button1.Top = top1;
+            button2.Left = left2;
+            button2.Top = top1;
+            button1.Text = l[l.Count -1];
+            this.Controls.Add(button1);
+            this.Controls.Add(button2);
+            top1 += button3.Height + 2;
+            button2.Size = button3.Size;
+            
+            Image newImage = Image.FromFile(l[l.Count - 1]);
+
+            button2.BackgroundImage = (Image)(newImage);
+
+
+
+        }
+
+ 
+
+        private void but_Click(object sender, EventArgs e)
+        {
+
+            String imageLocation = "";
+
+            //openFileDialog1.Filter = "jpg files(*.jpg)|*.jpg|PNG files(*.png)|*.png|GIF files(*.gif)|*.gif|";
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                imageLocation = openFileDialog1.FileName;
+                Image newImage = Image.FromFile(imageLocation);
+                this.button3.BackgroundImage = (Image)(newImage);
+                l.Add(imageLocation);
+                buttonAdd();
+
+
+            }
+        }
+    }
+
+  
+}
